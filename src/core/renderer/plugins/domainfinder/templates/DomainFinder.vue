dec9d21d7a6559fd9f66c40cc6b08dfea3ddb4f0
Marcus Johansson <me@marcusmailbox.com>
2019-09-17 08:18:17 +0200

    All photo forms done

--- src/core/renderer/plugins/domainfinder/templates/DomainFinder.vue
+++ src/core/renderer/plugins/domainfinder/templates/DomainFinder.vue
@@ -3,7 +3,11 @@
   <v-layout
     align-left
   >
-    <v-form>
+    <v-form
+      ref="form"
+      v-model="valid"
+      :lazy-validation="lazy"
+    >
       <v-container>
         <v-row>
           <v-col cols="12" sm="12" md="12">
@@ -11,24 +15,27 @@
                 label="Your brand name"
                 placeholder="Your brand name"
                 outlined
-              ></v-text-field>
+                v-model="name"
+              >
+            </v-text-field>
           </v-col>
 
           <v-col cols="12" sm="12" md="12">
             <v-text-field
                 label="Keywords"
-                placeholder="Keywords"
+                placeholder="Keywords (e.g. photo find)"
+                v-model="keywords"
                 outlined
               ></v-text-field>
           </v-col>
 
           <v-col cols="3" sm="3" md="3">
             <v-switch
-              v-model="findSynonyms"
+              v-model="useSynonyms"
               class="mt-0"
               color="blue lighten-2"
               hide-details
-              label="Find Synonyms"
+              label="Use Synonyms (e.g. polaroid-getter.com)"
             ></v-switch>
           </v-col>
 
@@ -38,7 +45,7 @@
               class="mt-0"
               color="blue lighten-2"
               hide-details
-              label="Allow EMD"
+              label="Allow EMD (e.g. photo-finder.com)"
             ></v-switch>
           </v-col>
 
@@ -52,17 +59,66 @@
             ></v-slider>
           </v-col>
 
-          <v-col cols="12" sm="12" md="12">
+          <v-col cols="3" sm="3" md="3">
+            <v-switch
+              v-model="useSuffixes"
+              class="mt-0"
+              color="blue lighten-2"
+              hide-details
+              label="Use Web Suffixes (e.g. photOFY, photR)"
+            ></v-switch>
+          </v-col>
+
+          <v-col cols="3" sm="3" md="3">
+            <v-switch
+              v-model="usePrefixes"
+              class="mt-0"
+              color="blue lighten-2"
+              hide-details
+              label="Use Prefixes (e.g. thephoto, bestphoto)"
+            ></v-switch>
+          </v-col>
+
+          <v-col cols="6" sm="6" md="6">
             <v-select
-              v-model="value"
-              :items="items"
+              v-model="tldValue"
+              :items="tldItems"
               label="Select TLD's"
               multiple
             >
             </v-select>
           </v-col>
+
+          <v-col cols="3" sm="3" md="3">
+            <v-switch
+              v-model="usePrefixes"
+              class="mt-0"
+              color="blue lighten-2"
+              hide-details
+              label="Use Dashes (e.g. find-photos.com)"
+            ></v-switch>
+          </v-col>
+
+          <v-col cols="3" sm="3" md="3">
+            <v-switch
+              v-model="useStemming"
+              class="mt-0"
+              color="blue lighten-2"
+              hide-details
+              label="Use Stemming (e.g. find-photos.com and find-photo.com)"
+            ></v-switch>
+          </v-col>
+
+          <v-btn
+            color="success"
+            class="mr-4"
+            @click="submit"
+          >
+            Find Domain Names
+          </v-btn>
         </v-row>
       </v-container>
+      
     </v-form>
   </v-layout>
 </template>
@@ -74,9 +130,25 @@
     },
     data: () => ({
       max: 15,
-      items: ['.com', '.org', '.net', '.edu', '.io', '.test'],
-      value: ['.com', '.org', '.net'],
+      tldItems: ['.com', '.org', '.net', '.edu', '.io', '.test'],
+      tldValue: ['.com', '.org', '.net'],
+      name: '',
+      valid: true,
+      lazy: false,
+      allowEmd: true,
+      useSynonyms: true,
+      usePrefixes: false,
+      useSuffixes: false,
+      useDashes: false,
+      useStemming: false,
+      useDashes: false,
+      keywords: ''
     }),
+    methods: {
+      submit () {
+        console.log(this.name)
+      }
+    },
     created () {
       this.$vuetify.theme.dark = true
     }
