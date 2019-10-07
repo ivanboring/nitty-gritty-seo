e23637a09df5647a5c4415749d2df8094bc8b597
Marcus Johansson <me@marcusmailbox.com>
2019-10-07 20:57:35 +0200

    Database insertion for auditing on projects

--- src/core/main/database/db20190928.js
+++ src/core/main/database/db20190928.js
@@ -5,7 +5,17 @@ var db20190928 = {
     ]
   },
   commandsProject () {
-    return []
+    return [
+      'CREATE TABLE `audit` ( `aid` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `started` INTEGER, `finished` INTEGER, `status` INTEGER, `errors` INTEGER, `warnings` INTEGER, `notifications` INTEGER )',
+      'CREATE TABLE `audit_page` ( `path` TEXT, `aid` INTEGER, `errors` INTEGER, `warnings` INTEGER, `notices` INTEGER )',
+      'CREATE TABLE "audit_messages" ( `aid` INTEGER, `path` TEXT, `status` INTEGER, `message` BLOB )',
+      'CREATE TABLE `audit_category` ( `cid` TEXT NOT NULL UNIQUE, `name` TEXT, `weight` INTEGER )',
+      'CREATE TABLE "audit_markup" ( `aid` INTEGER, `path` TEXT UNIQUE, `markup` BLOB, `type` INTEGER )',
+      'CREATE INDEX `audit_message_path_aid` ON `audit_messages` ( `aid`, `path` )',
+      'CREATE UNIQUE INDEX `path_aid` ON `audit_page` ( `path` ASC, `aid` ASC )',
+      'CREATE UNIQUE INDEX `audit_category_cid` ON `audit_category` ( `cid` ASC )',
+      'CREATE UNIQUE INDEX `audit_markup_path_type_aid` ON `audit_markup` ( `path` ASC, `type` ASC, `aid` ASC )'
+    ]
   }
 }
 
